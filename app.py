907978c714473de568323231f2dd42a0bbf52539
mimu <mirriamchibala@yahoo.com>
2025-08-10 14:02:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Chatbot API")
 
 # Change here to match your environment variable name
 HF_TOKEN = os.getenv("HF_TOKEN_2")  
-DEFAULT_MODEL = os.getenv("MODEL_NAME", "mistralai/Mistral-7B-Instruct-v0.3")
+DEFAULT_MODEL = os.getenv("MODEL_NAME", "EleutherAI/gpt-neo-2.7B")
 
 class GenerateRequest(BaseModel):
     prompt: str
